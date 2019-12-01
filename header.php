bbb3b7c16c5bd85bb61659d09eb402200aa655d8
Valentine APPY <vale.appy@gmail.com>
2019-12-01 21:06:01 +0100

    [v] - 01/12/2019 - Formulaire Newsletter sans CSS

---
+++ header.php
@@ -0,0 +1,19 @@
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  <title>Mosaic Association</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" />
+  <link rel="shortcut icon" type="image/x-icon" type="image/ico" href="img/favicon_color@2x.png" />
+  <link rel="apple-touch-icon" sizes="60x60 76x76 120x120 152x152" type="image/png" href="img/favicon_color@2x.png" />
+
+  <script type="text/javascript" src="ext//jquery-3.3.1.min.js"></script>
+
+  <script type="text/javascript" src="ext//jquery.min.js"></script>
+  <script type="text/javascript" src="ext/moment.min.js"></script>
+  <script type="text/javascript" src="ext/bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="ext/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
