22b7cd5d43db15cee3f40caad794ce38da44f5a0
Valentine APPY <vale.appy@gmail.com>
2019-12-04 23:09:56 +0100

    [V]  - 04/12 - HTML LP Mosaic

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Mosaic Association</title>
+  <title>Accueil - Mosaic Association</title>
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" />
+  
   <link rel="shortcut icon" type="image/x-icon" type="image/ico" href="img/favicon_color@2x.png" />
   <link rel="apple-touch-icon" sizes="60x60 76x76 120x120 152x152" type="image/png" href="img/favicon_color@2x.png" />
 
@@ -14,6 +15,5 @@
   <script type="text/javascript" src="ext/moment.min.js"></script>
   <script type="text/javascript" src="ext/bootstrap/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="ext/bootstrap/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="css/style.css">
-
+  <link rel="stylesheet" href="css/style.css">  
 </head>
